0d166e60b34121bc28b8ce34bc19997f62587601
Nisha Shlaymoon <nishatoma@gmail.com>
2019-02-16 18:28:12 -0500

    Update Recipe
    Update Recipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-list';
+
+  loadedFeature = 'recipe';
+
+  navigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
